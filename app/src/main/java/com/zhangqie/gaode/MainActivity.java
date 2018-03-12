9a40f91c8340b81455f04bf771292c32ecc53afa
DickyQie <18523978916@163.com>
2018-03-12 16:31:42 +0800

    android高德地图功能详解

---
+++ app/src/main/java/com/zhangqie/gaode/MainActivity.java
@@ -0,0 +1,57 @@
+package com.zhangqie.gaode;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.zhangqie.gaode.ui.basic.LocationActivity;
+
+
+/**
+ * Created by zhangqie on 2017/3/12.
+ *
+ * 高德地图功能列表
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String[] strList =new  String[]{
+            "定位数据，显示地图，标注位置"
+    };
+
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        listView = (ListView) findViewById(R.id.listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,strList);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showIntent(position);
+            }
+        });
+    }
+
+    private void showIntent(int index){
+        switch (index){
+            case 0:
+                startActivity(new Intent(MainActivity.this,LocationActivity.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/zhangqie/gaode/ui/route/RouteActivity.java
@@ -0,0 +1,12 @@
+package com.zhangqie.gaode.ui.route;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhangqie on 2017/8/12.
+ */
+
+public class RouteActivity extends AppCompatActivity {
+
+
+}
